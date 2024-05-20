92842b50153f33cbe8077712d350432e2b626328
mbc <dksdudwls888@naver.com>
2024-05-20 15:47:38 +0900

    404 완료

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -16,6 +16,10 @@ naming_convention = {
 # db = SQLAlchemy()
 db =  SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 def create_app():
     app = Flask(__name__)
@@ -51,5 +55,7 @@ def create_app():
 
     from .views import weather_views
     app.register_blueprint(weather_views.bp)
+
+    app.register_error_handler(404, page_not_found)
 
     return app
